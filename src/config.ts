98dfae9dc9c04a2f839b175b6a37d6f5d56bc035
luya0702 <2976977535@qq.com>
2026-06-08 12:51:45 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -44,6 +44,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
+		{
+			name: "新人必读",
+			url: "https://loagal.top/posts/jc/jcz/", // Internal links should not include the base path, as it is automatically added
+			external: false, // Show an external link icon and will open in a new tab
+		},
 	],
 };
 
